2b5ea5c358573bbd5aeb6642a38e308d35877221
Samael Will <doupe1995@gmail.com>
2022-10-05 17:59:49 +0300

    Example if

---
+++ Examples/Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Приветули ЕПТ!");
+}
+else
+{
+    Console.Write("Приветули, ");
+    Console.WriteLine(username);
+}
